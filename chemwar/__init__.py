f009c701f7c2c47e425da31ef4c9911bcb5ff332
Natte2110 <natteparsley@gmail.com>
2024-02-08 16:11:57 +0000

    feat: Add init file for chemwar app

---
+++ chemwar/__init__.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env  # noqa
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+# app.config['UPLOAD_FOLDER'] = os.environ.get("UPLOAD_FOLDER")
+db = SQLAlchemy(app)
+
+from chemwar import routes  # noqa
