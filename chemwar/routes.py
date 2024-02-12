10aa6359904f7ddf8e2c1afd56a4fabb3043365e
Natte2110 <natteparsley@gmail.com>
2024-02-12 14:07:28 +0000

    feat: Add routing for user logins

--- chemwar/routes.py
+++ chemwar/routes.py
@@ -1,15 +1,28 @@
 from flask import render_template, request, redirect, flash, url_for, jsonify
 from chemwar import app, db
-# from chemwar.models import User, Category, Post, Reaction, Comment
-# import flask_login as fl
+from chemwar.models import User, Cordon, CBRN
+import flask_login as fl
 from hashlib import sha256
 from datetime import datetime
 from sqlalchemy import desc
 import os
 
-# login_manager = fl.LoginManager(app)
-# login_manager.login_view = 'log_in'
+login_manager = fl.LoginManager(app)
+login_manager.login_view = 'sign_in'
 # PATH_TO_IMAGES = '/'.join(app.config['UPLOAD_FOLDER'].split("/")[-2:])
+
+@login_manager.user_loader
+def load_user(id):
+    """Loads the current user and returns it as a query object.
+
+    Arguments:
+        id -- The id of the user, this will be used to search the
+        USER database table for the corresponding entry.
+
+    Returns:
+        A User object returned from a query on the User table.
+    """
+    return User.query.get(int(id))
 
 @app.route("/")
 def home():
@@ -29,3 +42,34 @@ def chemwar_viewer():
     """
     return render_template("viewer.html", title="Viewer")
 
+@app.route("/sign-in", methods=["GET", "POST"])
+def sign_in():
+    """Provides routing for the website's sign in page
+
+    Returns:
+        The sign-in.html page with the title of "Sign In"
+    """
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        # encrypted_password = sha256(password.encode("utf-8")).hexdigest()
+        if user and (user.password == password):
+            fl.login_user(user)
+            # flash(f"Welcome {username}!")
+            return redirect(url_for('home'))
+        else:
+            return render_template("sign-in.html", title="Log In", error="t")
+    else:
+        return render_template("sign-in.html", title="Sign In")
+
+@app.route("/logout")
+@fl.login_required
+def logout():
+    """Logs the user out of their account
+
+    Returns:
+        The home page.
+    """
+    fl.logout_user()
+    return redirect(url_for('home'))
